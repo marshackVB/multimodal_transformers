1d670c06b9ad04205cbfa1f9152d575584397029
Marshall.Carter <carter.marshall@gmail.com>
2021-12-09 12:11:09 -0500

    added env var export and param

--- db_runner.sh
+++ db_runner.sh
@@ -2,6 +2,6 @@
 
 export MLFLOW_TRACKING_URI=databricks://e2-demo-east
 
-mlflow run https://github.com/marshackVB/ \
-    ./datasets/ecommerce_reviews/training_config.json \
+mlflow run https://github.com/marshackVB/multimodal_transformers \
+    -P config_file=./datasets/ecommerce_reviews/training_config.json \
     --backend-config cluster_config.json --experiment-id 648050317765410
