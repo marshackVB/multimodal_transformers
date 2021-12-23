3b7bd516fb6244392367817c665c3556d712635e
Marshall.Carter <carter.marshall@gmail.com>
2021-12-23 18:51:19 -0500

    added argparse

--- train.py
+++ train.py
@@ -21,20 +21,24 @@ import mlflow
 #%aimport transformations,  model, arguments
 
 
-def main():
+def main(data_args, model_args, training_args):
+
+    print("Main method is running")
 
     # Parse arguments
-    parser = HfArgumentParser([DataArguments, ModelArgs, MultiModelTrainingArgs])
+    
 
     # Temp load json config
     #data_args, model_args, training_args = parser.parse_json_file(json_file=os.path.abspath("datasets/ecommerce_reviews/training_config.json"))
 
+    """
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         data_args, model_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
     else:
         raise Exception("You must provide a json configuration file as the single argument to the program")
 
     data_args, model_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    """
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
@@ -54,11 +58,13 @@ def main():
 
 
     # Transform features
-    transformer = (get_transformer(model_type =       model_args.model_name_or_path, 
-                                   text_cols =        data_args.column_info["text_cols"],
-                                   numeric_cols =     data_args.column_info["num_cols"], 
-                                   categorical_cols = data_args.column_info["cat_cols"])
-                                    .fit(train_df))
+    transformer = (get_transformer(model_type =           model_args.model_name_or_path, 
+                                   text_cols =            data_args.column_info["text_cols"],
+                                   numeric_cols =         data_args.column_info["num_cols"], 
+                                   categorical_cols =     data_args.column_info["cat_cols"],
+                                   max_length =           model_args.max_length, 
+                                   tokenizer_batch_size = model_args.tokenizer_batch_size)
+                                   .fit(train_df))
 
     label = data_args.column_info['label_col']
 
@@ -109,7 +115,7 @@ def main():
             runtime_version="local"
 
 
-        params = {"eval_batch_size":        trainer.args.eval_batch_size,
+        params = {"eval_batch_size":       trainer.args.eval_batch_size,
                  "train_batch_size":       trainer.args.train_batch_size,
                  "gpus":                   trainer.args._n_gpu,
                  "epochs":                 trainer.args.num_train_epochs,
@@ -133,5 +139,38 @@ def main():
     #trainer.save_model()
 
 if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--model_name_or_path",             help="Pretrained model type")
+    parser.add_argument("--output_dir",                     help="Model checkpoint directory")
+    parser.add_argument("--column_info_path",               help="Model checkpoint directory")
+    parser.add_argument("--experiment_name",                help="Name of MLflow experiment")
+    parser.add_argument("--train_data_path_or_table_name",  help="Location of training data file")
+    parser.add_argument("--test_data_path_or_table_name",   help="Location of test data file")
+    parser.add_argument("--databricks_profile",             help="Databricks CLI profile for authentication")
+    parser.add_argument("--num_labels",                     help="Number of labels to predict")
+    parser.add_argument("--max_length",                     help="Number of tokens to retain")
+    parser.add_argument("--tokenizer_batch_size",           help="Batch size for tokenizer")
+    parser.add_argument("--num_train_epochs",               help="Number of training epochs")
+    parser.add_argument("--per_device_train_batch_size",    help="Training batch size")
+    parser.add_argument("--per_device_eval_batch_size",     help="Evaluation batch size")
+    parser.add_argument("--save_strategy",                  help="Checkpoint save strategy")
+    parser.add_argument("--save_total_limit",               help="Maximum number of saved checkpoints")
+    parser.add_argument("--evaluation_strategy",            help="Evaluation strategy")
+    parser.add_argument("--logging_strategy",               help="Logging strategy")
+    parser.add_argument("--logging_steps",                  help="Number of logging steps")
 
-    main()
+    """
+    args = parser.parse_args()
+    print("Training model with arguments:")
+    for arg in vars(args):
+        print(f"  {arg}: {getattr(args, arg)}")
+    """
+
+    hf_argparser = HfArgumentParser([DataArguments, ModelArgs, MultiModelTrainingArgs])
+
+    #data_args, model_args, training_args = hf_argparser.parse_args_into_dataclasses(args=sys.argv[1:], return_remaining_strings=True)
+    #data_args, model_args, training_args = hf_argparser.parse_args_into_dataclasses(args=sys.argv[1:], look_for_args_file=False)
+    data_args, model_args, training_args = hf_argparser.parse_args_into_dataclasses(look_for_args_file=False)
+
+    main(data_args, model_args, training_args)
