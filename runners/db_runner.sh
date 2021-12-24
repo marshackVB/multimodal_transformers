79be32b2fc7aea800a0c73baaa87ba9ac264667c
Marshall.Carter <carter.marshall@gmail.com>
2021-12-24 12:01:25 -0500

    updates

--- runners/db_runner.sh
+++ runners/db_runner.sh
@@ -2,8 +2,10 @@
 
 export MLFLOW_TRACKING_URI=databricks://e2-demo-east
 
+EXPERIMENT_ID=`cat runners/DB_EXPERIMENT_ID`
+
 mlflow run https://github.com/marshackVB/multimodal_transformers \
-    -P max_length=100 -P num_train_epochs=3 -P per_device_train_batch_size=128 -P per_device_eval_batch_size=128 \
+    -P max_length=100 -P num_train_epochs=10 -P per_device_train_batch_size=128 -P per_device_eval_batch_size=128 \
     --backend databricks \
     --backend-config cluster_config.json \
-    --experiment-id 648050317765410
+    --experiment-id $EXPERIMENT_ID
--- runners/local_runner.sh
+++ runners/local_runner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mlflow run ./ -P max_length=50 -P num_train_epochs=1 --experiment-id 0
+mlflow run ./ -P max_length=50 -P num_train_epochs=5 -P training_sample_record_num=200 --experiment-id 0
