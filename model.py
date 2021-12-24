8c6b4eca9daac3f2a047650419d246c00beabc36
Marshall.Carter <carter.marshall@gmail.com>
2021-12-23 22:38:59 -0500

    removed legacy sections

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple, Dict
+import json
 import torch
 from torch import nn
 from transformers import DistilBertForSequenceClassification
@@ -45,21 +47,6 @@ class MultiModalLoader(torch.utils.data.Dataset):
         attention_mask = attention_mask_tensors(x_train_row, self.attention_mask_col)
 
         num_cat_features = num_cat_feature_tensors(x_train_row, self.num_cat_feature_cols)
-
-        """
-        input_ids = (torch.tensor(x_train_row[self.input_ids_col])
-                          .to(torch.int64)
-                          .to(device))
-
-        attention_mask = (torch.tensor(x_train_row[self.attention_mask_col])
-                               .to(torch.int64)
-                               .to(device))
-
-        num_cat_features = (torch.tensor(x_train_row[self.num_cat_feature_cols]
-                                 .to_numpy(dtype='float32'))
-                                 .float()
-                                 .to(device))
-        """
         
         label = (torch.tensor(self.y_train.iloc[idx])
                       .unsqueeze(0)
@@ -146,17 +133,52 @@ class MultiModalTransformer(DistilBertForSequenceClassification):
         return softmax(logits)
 
 
-    def weight_init(self,  m, activation='linear'):
-        torch.nn.init.xavier_uniform_(m.weight, gain=nn.init.calculate_gain(activation))
-        return m
+def weight_init(self,  m, activation='linear'):
+    torch.nn.init.xavier_uniform_(m.weight, gain=nn.init.calculate_gain(activation))
+    return m
 
 
 def compute_metrics(pred):
-        labels = pred.label_ids
-        preds = pred.predictions.argmax(-1)
-        precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='weighted')
-        return {
-            'f1': f1,
-            'precision': precision,
-            'recall': recall
-                }
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='weighted')
+    return {
+        'f1': f1,
+        'precision': precision,
+        'recall': recall
+            }
+
+
+def get_best_metrics(trainer) -> Dict[str, float]:
+    """
+    Extract metrics from a fitted Trainer instance.
+    Args:
+    trainer: A Trainer instance that has been trained on data.
+
+    Returns:
+    A dictionary of metrics and their values.
+    """
+
+    # Best model metrics
+    best_checkpoint = f'{trainer.state.best_model_checkpoint}/trainer_state.json' 
+
+    with open(best_checkpoint) as f:
+        metrics = json.load(f)
+
+    best_step = metrics['global_step']
+
+    all_log_history = enumerate(metrics['log_history'])
+
+    best_log_idx = [idx for idx, values in all_log_history if values['step'] == best_step][0]
+
+    best_log = metrics['log_history'][best_log_idx]
+    best_log.pop('epoch')
+
+    # Overal runtime metrics
+    runtime_logs_idx = [idx for idx, values in enumerate(trainer.state.log_history) if values.get('train_runtime') is not None][0]
+    runtime_logs = trainer.state.log_history[runtime_logs_idx]
+
+    best_log['train_runtime'] = runtime_logs['train_runtime']
+    best_log['train_loss'] = runtime_logs['train_loss']
+
+    return best_log
